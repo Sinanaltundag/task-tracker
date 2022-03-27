e68bcc3e9aac598f869c006083129314620a8407
peykani <peykani@gmail.com>
2022-03-28 00:43:51 +0300

    fixed isdoneprop but unclean code

--- src/components/Tasks/Task.jsx
+++ src/components/Tasks/Task.jsx
@@ -1,35 +1,32 @@
 import {useState} from 'react'
 
-const Task = ({taskProps,isDoneProp,delTaskProp,dataProp}) => {
+const Task = (taskProps) => {
 
-const [isDone, setIsDone] = useState(dataProp.isDone)
+const [isDone, setIsDone] = useState(taskProps.taskProps.isDone)
 
     console.log(taskProps)
-    console.log(isDoneProp)
-    console.log(delTaskProp)
-    console.log(dataProp)
 
     const handleDelete = (e)=> {
       console.log(e)
       e.stopPropagation()
-      delTaskProp(taskProps.taskId)
+      taskProps.delTaskProp(taskProps.taskProps.taskId)
     }
 const handleIsDone =()=>{
   setIsDone(!isDone)
-  // taskProps.isDone=isDone
-  isDoneProp(taskProps.taskId)
+  taskProps.taskProps.isDone=isDone
+  taskProps.isDoneProp(taskProps.taskProps.taskId)
 }
 
-  return (<div>
+  return (
     <div className={`task ${isDone ? 'task-done':""}`} onClick={handleIsDone}>
     <div className="task-group">
-        <h3 className={`task-title ${isDone ? 'done':""}`}>{taskProps.taskTitle}</h3>
+        <h3 className={`task-title ${isDone ? 'done':""}`}>{taskProps.taskProps.taskTitle}</h3>
         <span onClick={handleDelete}>X</span>
     </div>
         
-        <p className={`task-date ${isDone ? 'done':""}`}>{taskProps.taskDate}</p>
+        <p className={`task-date ${isDone ? 'done':""}`}>{taskProps.taskProps.taskDate}</p>
         
-    </div></div>
+    </div>
   )
 }
 
